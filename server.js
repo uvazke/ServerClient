eb1af7a88f951d674d0bf9912c01dde2811ea27a
uvazke <kazu.verb.1123@gmail.com>
2018-01-06 14:11:06 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+//サーバ作成
+var http = require('http');
+var server = http.createServer(doRequest);
+var io = require('socket.io').listen(server);
+var fs = require('fs');
+var url = require("url");
+//server.on('request', doRequest);
+server.listen(8810);
+console.log('server runnning');
+//クライアント接続があると、以下の処理をさせる。
+io.sockets.on('connection', function (socket) {
+  console.log('connected');
+  //接続通知をクライアントに送信
+  io.emit("sendMessageToClient", {value:"1人入室しました。"});
+
+  //クライアントからの受信イベントを設定
+  socket.on("sendMessageToServer", function (data) {
+      io.emit("sendMessageToClient", {value:data.value});
+  });
+
+  //接続切れイベントを設定
+  socket.on("disconnect", function () {
+      io.emit("sendMessageToClient", {value:"1人退室しました。"});
+  });
+});
+
+function doRequest(req,res){
+  fs.readFile('./client.html', 'UTF-8',
+  function(err, data) {
+    var path = url.parse(req.url).pathname;
+    console.log("Request for " + path + "received.");
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}
